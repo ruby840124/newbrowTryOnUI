acdb776f48fbcd43081a3e4cf18243da7e957bc6
ruby840124 <ruby840124@gmail.com>
2019-10-17 09:58:46 +0800

    Updates

---
+++ precache-manifest.700e1e89938cfcf41421b75ee13cfd1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f10fb6baf035ace48e4577e70e5b12ab",
+    "url": "/newbrowTryOnUI/index.html"
+  },
+  {
+    "revision": "a4d1b59367458ff59e8e",
+    "url": "/newbrowTryOnUI/static/css/main.f38db748.chunk.css"
+  },
+  {
+    "revision": "e21a4efefcc065e31c4c",
+    "url": "/newbrowTryOnUI/static/js/2.98236c1f.chunk.js"
+  },
+  {
+    "revision": "a4d1b59367458ff59e8e",
+    "url": "/newbrowTryOnUI/static/js/main.aa247601.chunk.js"
+  },
+  {
+    "revision": "61eb62274930c117a1bc",
+    "url": "/newbrowTryOnUI/static/js/runtime-main.9b6ce39d.js"
+  },
+  {
+    "revision": "a2797f4e4438cfc6deea1bef5b8e750b",
+    "url": "/newbrowTryOnUI/static/media/after.a2797f4e.jpg"
+  }
+]);
